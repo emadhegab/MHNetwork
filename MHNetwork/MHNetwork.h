1c2be1912db31c328bb329ca6ded200feb2917f7
Mohamed Emad Abdalla Hegab <mohamed.hegab@zalando.de>
2018-07-16 13:45:14 +0200

    Initial Commit

---
+++ MHNetwork/MHNetwork.h
@@ -0,0 +1,19 @@
+//
+//  MHNetwork.h
+//  MHNetwork
+//
+//  Created by Mohamed Emad Abdalla Hegab on 16.07.18.
+//  Copyright © 2018 Mohamed Hegab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MHNetwork.
+FOUNDATION_EXPORT double MHNetworkVersionNumber;
+
+//! Project version string for MHNetwork.
+FOUNDATION_EXPORT const unsigned char MHNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHNetwork/PublicHeader.h>
+
+
