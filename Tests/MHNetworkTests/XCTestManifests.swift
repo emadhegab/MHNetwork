8e0b7d31be4272e55f1697a52b78c31a4614ca3d
Mohamed Emad Hegab <emadhegab@gmail.com>
2019-11-25 17:02:22 +0100

    fix: test cases

--- Tests/MHNetworkTests/XCTestManifests.swift
+++ Tests/MHNetworkTests/XCTestManifests.swift
@@ -1,9 +1,9 @@
-import XCTest
-
-#if !os(macOS)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(MHNetworkTests.allTests),
-    ]
-}
-#endif
+//import XCTest
+//
+//#if !os(macOS)
+//public func allTests() -> [XCTestCaseEntry] {
+//    return [
+//        testCase(MHNetworkTests.allTests),
+//    ]
+//}
+//#endif
